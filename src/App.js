580f8af39264522ec9320962f5f603ff96f5d884
Taty <tatyanaparks@gmail.com>
2021-03-23 14:05:40 -0500

    navbar edits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import './App.css';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Shop from "./views/Shop";
+// import Shop from "./views/Shop";
 import Contact from "./views/Contact";
 import Home from "./views/Home";
+import Shop from "./views/Shop"
 
 export default class App extends Component {
   constructor() {
@@ -26,9 +27,9 @@ export default class App extends Component {
         </header>
         <main className="container">
           <Switch>
-            <Route exact path='/' render={() => <Home />} />
-            <Route exact path='/contact' render={() => <Contact />} />
-            <Route exact path='/shop' render={() => <Shop />} />
+            <Route exact path="/" render={() => <Home />} />
+            <Route exact path="/contact" render={() => <Contact />} />
+            <Route exact path="/shop" render={() => <Shop />} />
           </Switch>
         </main>
         <footer></footer>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,66 +1,90 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import logo from "./rootin4uslogo.png"
 
 export default class Navbar extends Component {
   render() {
     return (
-      <div className="header-container">
-        <nav className="myNavbar">
-          <ul className="navbar_container">
-            <li className="navbar-item-content-first">
-              <Link to="/">
-                <div className="navbar-item-content">
-                  <span class="menu-icon">
-                    <i class="fa fa-home fa-2x"></i>
+      <>
+        <div className="header-container">
+          <nav className="myNavbar">
+            <ul className="navbar_container">
+              <li className="navbar-item-content-first">
+                <Link to="/">
+                  <div className="navbar-item-content">
+                    <span class="menu-icon">
+                      <i class="fa fa-home fa-2x"></i>
+                    </span>
+                    Home
+                  </div>
+                </Link>
+              </li>
+              <div class="dropdown">
+                <button class="dropbtn">
+                  <span className="menu-icon">
+                    <i className="fa fa-carrot fa-2x" />
                   </span>
-                  Home
+                  Buy Local Food
+                </button>
+                <div class="dropdown-content">
+                  <a href="/">Products</a>
+                  <a href="/">Cart</a>
+                  <a href="/">Checkout</a>
                 </div>
-              </Link>
-            </li>
-            <li className="navbar-item-content">
-              <Link to="localfood">
-                <span className="menu-icon">
-                  <i className="fa fa-carrot fa-2x" />
+              </div>
+              {/* <li className="navbar-item-content">
+                <Link to="localfood">
+                  <span className="menu-icon">
+                    <i className="fa fa-carrot fa-2x" />
+                  </span>
+                  Buy Local Food
+                </Link>
+              </li> */}
+              <li className="navbar-item-content">
+                <Link to="recipes">
+                  <span className="menu-icon">
+                    <i className="fa fa-scroll fa-2x" />
+                  </span>
+                  Recipes
+                </Link>
+              </li>
+              <li className="navbar-item-logo">
+                <span className="logo-holder">
+                  <img src={logo} alt="logo" />
                 </span>
-                Buy Local Food
-              </Link>
-            </li>
-            <li className="navbar-item-content">
-              <Link to="recipes">
-                <span className="menu-icon">
-                  <i className="fa fa-scroll fa-2x" />
-                </span>
-                Recipes
-              </Link>
-            </li>
-            <li className="navbar-item-content">
-              <Link to="reviews">
-                <span className="menu-icon">
-                  <i className="fas fa-thumbs-up fa-2x" />
-                </span>
-                Reviews
-              </Link>
-            </li>
-            <li className="navbar-item-content">
-              <Link to="customerlogin">
-                <span className="menu-icon">
-                  <i className="fas fa-sign-in-alt fa-2x" />
-                </span>
-                Customer Login
-              </Link>
-            </li>
-            <li className="navbar-item-content">
-              <Link to="sellerlogin">
-                <span className="menu-icon">
-                  <i className="fas fa-store fa-2x" />
-                </span>
-                Seller Login
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+              </li>
+              <li className="navbar-item-content">
+                <Link to="reviews">
+                  <span className="menu-icon">
+                    <i className="fas fa-thumbs-up fa-2x" />
+                  </span>
+                  Reviews
+                </Link>
+              </li>
+              <li className="navbar-item-content">
+                <Link to="customerlogin">
+                  <span className="menu-icon">
+                    <i className="fas fa-sign-in-alt fa-2x" />
+                  </span>
+                  Customer Login
+                </Link>
+              </li>
+              <li className="navbar-item-content">
+                <Link to="sellerlogin">
+                  <span className="menu-icon">
+                    <i className="fas fa-store fa-2x" />
+                  </span>
+                  Seller Login
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <hr className="navbar-break-line" />
+      </>
     );
   }
 }
-      
+
+
